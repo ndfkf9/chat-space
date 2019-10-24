709215ebc902acc63991e1a59cff3ca54f4c5bd0
ndfkf9 <sasasirokuro@gmail.com>
2019-10-24 11:23:48 +0900

    文字が入力されるたびにイベントが呼び出されるように記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,3 +48,11 @@ $(function(){
    return false;
   })
 });
+
+$(function(){
+
+  $('#user-search-field').on('keyup', function(e){
+    e.preventDefault();
+    var input = $('#user-search-field').val();    //フォームの値を取得して変数に代入する
+  })
+});
