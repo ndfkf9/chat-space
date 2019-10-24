f2b8b832135f79e907dc9c99c6f107e7632a8016
ndfkf9 <sasasirokuro@gmail.com>
2019-10-24 15:32:41 +0900

    非同期通信でユーザーを検索できるようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,8 +51,13 @@ $(function(){
 
 $(function(){
 
-  $('#user-search-field').on('keyup', function(e){
-    e.preventDefault();
+  $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();    //フォームの値を取得して変数に代入する
+    $.ajax({
+      type: "GET",    //HTTPメソッド
+      url: '/users',       //users_controllerの、indexアクションにリクエストの送信先を設定する
+      dataType: 'json',
+      data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+    })
   })
 });
