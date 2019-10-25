b83378f66f13f374ed45653d7b54f67c0ea96381
ndfkf9 <sasasirokuro@gmail.com>
2019-10-25 10:22:41 +0900

    Hamlの描画

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,15 +49,57 @@ $(function(){
   })
 });
 
-$(function(){
 
-  $('#user-search-field').on('keyup', function(){
-    var input = $('#user-search-field').val();    //フォームの値を取得して変数に代入する
+$(function() {
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".ChatMembers").append(html);
+  }
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();    //フォームの値を取得して変数に代入する
     $.ajax({
       type: "GET",    //HTTPメソッド
-      url: '/users',       //users_controllerの、indexアクションにリクエストの送信先を設定する
-      dataType: 'json',
+      url: "/users",       //users_controllerの、indexアクションにリクエストの送信先を設定する
       data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+      dataType: "json"
     })
-  })
+      .done(function(users) {
+        $("#user-search-result").empty();  //emptyメソッドで一度検索結果を空にする
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
+  });
 });
